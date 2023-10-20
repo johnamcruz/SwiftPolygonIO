3fdf53faa599537104f30d4deb16589c8632dda6
John Cruz <johnamcruz@gmail.com>
2023-10-19 19:21:09 -1000

    add api key support

--- Sources/PolygonClient/Transport/UrlSessionTransport.swift
+++ Sources/PolygonClient/Transport/UrlSessionTransport.swift
@@ -10,21 +10,24 @@ import Foundation
 @available(macOS 12.0, *)
 class UrlSessionTransport: HttpTransport {
     private let session: URLSession
-    internal var header: [String : String] = [:]
+    internal var headers: [String : String] = [:]
     private let decoder: JSONDecoder
     
     
     init(session: URLSession = URLSession.shared, 
-         header: [String : String] = [:],
+         headers: [String : String] = [:],
          decoder: JSONDecoder = JSONDecoder()) {
         self.session = session
-        self.header = header
+        self.headers = headers
         self.decoder = decoder
     }
     
     func send<Response: Codable>(request: URLRequest) async throws -> Response {
+        var request = request
+        for header in headers {
+            request.addValue(header.value, forHTTPHeaderField: header.key)
+        }
         let (data,_) = try await self.session.data(for: request)
         return try decoder.decode(Response.self, from: data)
     }
-    
 }
